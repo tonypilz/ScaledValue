e57086b4cee4847b28930ab9b3328e0a1242a884
Tony Pilz <tonypilzj@gmx.de>
2017-06-23 13:04:58 +0200

    fixed adl lookup

--- devel/src/ScaledValueMath.h
+++ devel/src/ScaledValueMath.h
@@ -8,30 +8,34 @@
 namespace sv {
 namespace math {
 
-
+using std::ratio;
+using std::abs;
+using std::sqrt;
+using std::pow;
+using std::conditional;
 
 template <typename S>
-using AbsRatio = typename std::conditional<(S::num>0) ,S , std::ratio<-S::num,S::den>>::type;
+using AbsRatio = typename conditional<(S::num>0) ,S , ratio<-S::num,S::den>>::type;
 
 template <typename S, typename T>
-constexpr ScaledValue<AbsRatio<S>, decltype(std::abs(T{}))>
+constexpr ScaledValue<AbsRatio<S>, decltype(abs(T{}))>
 abs(ScaledValue<S, T> const& q) {
-    return ScaledValue<AbsRatio<S>, decltype(std::abs(T{}))>{
-                                              std::abs(q.value())};
+    return ScaledValue<AbsRatio<S>, decltype(abs(T{}))>{
+                                              abs(q.value())};
 }
 
 template <typename S, typename T>
-constexpr ScaledValue<S,decltype(std::sqrt(T{}*S::num/S::den)*S::den/S::num)>
+constexpr ScaledValue<S,decltype(sqrt(T{}*S::num/S::den)*S::den/S::num)>
 sqrt(ScaledValue<S, T> const& q) {
-    return ScaledValue<S, decltype(std::sqrt(q.value()*S::num/S::den)*S::den/S::num)>{
-                                    std::sqrt(q.value()*S::num/S::den)*S::den/S::num};
+    return ScaledValue<S, decltype(sqrt(q.value()*S::num/S::den)*S::den/S::num)>{
+                                    sqrt(q.value()*S::num/S::den)*S::den/S::num};
 }
 
 template <typename S, typename T>
-constexpr ScaledValue<S,decltype(std::pow(T{}*S::num/S::den,T{})*S::den/S::num)>
+constexpr ScaledValue<S,decltype(pow(T{}*S::num/S::den,T{})*S::den/S::num)>
 pow(ScaledValue<S, T> const& q, T const& exponent ) {
-    return ScaledValue<S, decltype(std::pow(q.value()*S::num/S::den,exponent)*S::den/S::num)>{
-                                    std::pow(q.value()*S::num/S::den,exponent)*S::den/S::num};
+    return ScaledValue<S, decltype(pow(q.value()*S::num/S::den,exponent)*S::den/S::num)>{
+                                    pow(q.value()*S::num/S::den,exponent)*S::den/S::num};
 }
 
 
